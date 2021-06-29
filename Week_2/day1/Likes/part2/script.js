d2c859a5c8c25c7ba52e2254daff6c6f2fd65b5d
jencamara <jencamagong@gmail.com>
2021-06-28 18:21:58 -0700

    W2D6

---
+++ Week_2/day1/Likes/part2/script.js
@@ -0,0 +1,32 @@
+var likenumber1 = 9;
+var likenumber1Element = document.querySelector("#likenumber1");
+
+console.log(likenumber1Element)
+
+function plus1() {
+    likenumber1++;
+    likenumber1Element.innerText = likenumber1 + " likes";
+    console.log(likenumber1);
+}
+
+var likenumber2 = 12;
+var likenumber2Element = document.querySelector("#likenumber2");
+
+console.log(likenumber2Element)
+
+function add1() {
+    likenumber2++;
+    likenumber2Element.innerText = likenumber2 + " likes";
+    console.log(likenumber2);
+}
+
+var likenumber3 = 9;
+var likenumber3Element = document.querySelector("#likenumber3");
+
+console.log(likenumber3Element)
+
+function increase1() {
+    likenumber3++;
+    likenumber3Element.innerText = likenumber3 + " likes";
+    console.log(likenumber3);
+}
