b50d3808b427ba56daf2b2705b6babbb5f8c722a
jencamara <jencamagong@gmail.com>
2021-06-29 20:05:06 -0700

    W2D7 Commit

---
+++ Week_2/day7/Dojo_Weather/script.js
@@ -0,0 +1,15 @@
+
+
+function degreesCel() {
+    var tempuratureElement = document.querySelector("degreesc");
+    console.log('degreesc');
+}
+
+function degreeFar() {
+    var tempuratureElement = document.querySelector("degreesf");
+    console.log('degreesf');
+}
+
+function hide(element) {
+    element.remove();
+}
